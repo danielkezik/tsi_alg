5c94ce3a96467ef6f803fa4e2b1fe022041ea387
danielkezik <daniel.kezik@gmail.com>
2018-11-21 18:21:59 +0200

    Initial commit

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Lab4. Daniels Keziks. 4701BD. 21.11.2018");
+        }
+    }
+}
